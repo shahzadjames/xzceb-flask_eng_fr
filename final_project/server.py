873c1e7dfe0184597e3b41f427993a4346b43d42
shahzadjames <shahzadjamesffbl@gmail.com>
2023-01-18 14:05:24 -0500

    correct french to english reverse order

--- final_project/server.py
+++ final_project/server.py
@@ -8,14 +8,14 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    french_text = translator.frenchToEnglish(textToTranslate)
+    french_text = translator.englishToFrench(textToTranslate)
     return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    english_text = translator.englishToFrench(textToTranslate)
+    english_text = translator.frenchToEnglish(textToTranslate)
     return english_text
 
 @app.route("/")
